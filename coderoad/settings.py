101089032e27e37e7d58cfcd32df59413b4cd2bf
TaigaMikami <mikamimikami60@gmail.com>
2018-07-08 21:06:02 +0900

    change db to mysql

--- coderoad/settings.py
+++ coderoad/settings.py
@@ -78,8 +78,19 @@ WSGI_APPLICATION = 'coderoad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coderoad',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        'TEST': {
+            'NAME': 'test_sample'
+        }
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
