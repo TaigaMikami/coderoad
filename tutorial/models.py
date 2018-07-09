422ab9f9b5dec8e42a0c09ba1a395e9ff19cbecf
TaigaMikami <mikamimikami60@gmail.com>
2018-07-09 12:26:33 +0900

    コメントモデルの追加

--- tutorial/models.py
+++ tutorial/models.py
@@ -23,3 +23,13 @@ class Tutorial(models.Model):
   def __str__(self):
     return self.name
 
+class Comment(models.Model):
+  '''チュートリアルに対するコメント'''
+  name = models.CharField('お名前', max_length=30, default='名無し')
+  text = models.TextField('本文')
+  tutorial = models.ForeignKey(Tutorial, verbose_name='紐づくチュートリアル', on_delete=models.PROTECT)
+  created_at = models.DateField('作成日', default=timezone.now)
+  
+  def __str__(self):
+    return self.text[:10]
+  
